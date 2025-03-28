8772b0095f04612ac882b28f1269ee2f492df08d
Andrii <andrii.rozhanskyy@sifex-tech.com>
2025-03-28 09:10:24 +0200

    fixes ts

--- src/app/(users)/[role]/[id]/profiles/page.tsx
+++ src/app/(users)/[role]/[id]/profiles/page.tsx
@@ -3,7 +3,7 @@ import { Profiles } from "@/components/profile/Profiles";
 import { useSelector } from "react-redux";
 import { RootState } from "@/types/RootState";
 
-export const ProfilePage = () => {
+const ProfilePage = () => {
   const userData = useSelector((state: RootState) => state.user.user);
 
   return (
